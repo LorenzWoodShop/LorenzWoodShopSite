e7a549ae2fca2f902f360c37544811a3e200c6ee
Grayson Lorenz <lunarfuror@gmail.com>
2021-05-15 20:56:11 -0500

    Fixing the transition garbage

--- src/views/WoodCostCalculator/WoodCostCalculator.vue
+++ src/views/WoodCostCalculator/WoodCostCalculator.vue
@@ -1,26 +1,21 @@
 <template>
-  <div class="full-screen">
+  <div class="full-size">
     <h1>Board Cost Calculator</h1>
     <div class="grid --mobile --col-2-even --c-gap-medium">
       <div>
-        <div class="card grid --col-3-auto --c-gap-tiny --r-gap-tiny --full-center">
+        <div class="card grid --col-2-auto --c-gap-tiny --r-gap-tiny --full-center">
           <span class="h1 card__header --full-width">Add a board</span>
           <label class="error --full-width" v-if="errorMessage.trim() !== ''">{{ errorMessage }}</label>
           <label>Species</label>
           <input v-model="boardName" />
-          <div><!-- Spacer --></div>
           <label>Cost per BF</label>
           <input v-model="boardCost" type="number" step=".01" />
-          <span>$ / BF</span>
-          <label>Width</label>
+          <label>Width (in.)</label>
           <input v-model="boardWidth" type="number" step=".01" />
-          <span>in.</span>
-          <label>Length</label>
+          <label>Length (in.)</label>
           <input v-model="boardLength" type="number" step=".01" />
-          <span>in.</span>
-          <label>Thickness</label>
+          <label>Thickness (in.)</label>
           <input v-model="boardThickness" type="number" step=".01" />
-          <span>in.</span>
           <button class="--full-width" @click="AddBoardToCart()">Add</button>
         </div>
       </div>
